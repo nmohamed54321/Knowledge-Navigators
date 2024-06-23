cd7bdb6e40a84196d035147c7d0f01293075f8ad
brieey <briannanichole247@gmail.com>
2024-06-23 02:37:52 -0500

    Made migration changes

--- learnovation/learnovation_container/settings.py
+++ learnovation/learnovation_container/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'learnovation_launchpad.wsgi.application'
+WSGI_APPLICATION = 'learnovation_container.wsgi.application'
 
 
 # Database
