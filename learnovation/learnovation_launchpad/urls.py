e1e79805a09220eb81aceb4d80b3721a6a5fdf92
Rachel Estilo <raystilo4@gmail.com>
2024-06-27 12:08:19 -0500

    RE - Reorganized folders to match main branch

--- learnovation/learnovation_launchpad/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-#If you have any specific routes or views you want to add, you can define them in the
-# urls.py and the views.py files in the learnovation_launchpad directory.
-
-urlpatterns = [
-    path(),
-]
