66bba13dec64e298caae7b6b7d53181ffa29ce79
brieey <briannanichole247@gmail.com>
2024-07-06 19:48:47 -0500

    added popup message for form and fixed database

--- learnovation/learnovation_launchpad/admin.py
+++ learnovation/learnovation_launchpad/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+from django.contrib import admin #This line imports the admin module
+from .models import ContactForm #This line imports the ContactForm 
+
 
 # Register your models here.
+admin.site.register(ContactForm)
+
